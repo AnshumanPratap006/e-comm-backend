b52de4b22f9415d91728b6b0693e9ecbea0f28dc
AnshumanPratap006 <anshumanz699@gmail.com>
2024-06-09 20:46:44 +0530

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/e-commerce');
+mongoose.connect('mongodb+srv://anshumansinghz06:ela6fl4sYOfYYMGJ@cluster0.e8ri0vs.mongodb.net/');
